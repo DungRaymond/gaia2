f5de7078e7b2e22b84cb5fdc296db3944b9f5bce
yvesNg <45696769+DungRaymond@users.noreply.github.com>
2023-07-26 10:14:21 +0700

    fast commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { Grid } from '@svelteuidev/core';
+  import Fa from 'svelte-fa/src/fa.svelte';
+  import { faPhone } from '@fortawesome/free-solid-svg-icons'
+</script>
+
+<div id="navbar">
+  <Grid>
+    <Grid.Col span={1} class="navbar-ele">
+      <Fa icon={faPhone} size={'0.8x'} />
+      0364343213
+    </Grid.Col>
+  </Grid>
+</div>
+
+<style>
+  .navbar-ele {
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  }
+</style>
